cf53a276d2aad6a5fbe1d4a04ea55ef7926f0d72
i-kk65536 <lockegh512@gmail.com>
2025-07-15 08:59:40 +0900

    feat: out default portfolio

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,1 +1,3 @@
 // place files you want to import through the `$lib` alias in this folder.
+export { default as ProjectCard } from './components/ProjectCard.svelte'
+export { default as SkillsGrid } from './components/SkillsGrid.svelte'
