b5b9b034b93c7873aab52141d0450b02e340a258
i-kk65536 <lockegh512@gmail.com>
2025-07-15 09:21:18 +0900

    chore: mswの削除

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,6 @@
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
-import msw from '@iodigital/vite-plugin-msw'
-import { handlers } from './src/mocks/handlers.js'
 
 export default defineConfig({
-	plugins: [sveltekit(), msw({ handlers }),
-	],
+	plugins: [sveltekit()],
 })
